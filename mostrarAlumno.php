853fa167e9748d2194a9d627791e029c734e28d8
AlvaroGomez19 <38272010+AlvaroGomez19@users.noreply.github.com>
2018-04-11 18:25:02 -0500

    sfdsf

---
+++ mostrarAlumno.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Datos</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+   <?php
+   $matricula=$_GET['matricula'];
+   $appat=$_GET['apellidop'];
+   $apmat=$_GET['apellidom'];
+   $nombre=$_GET['nombre'];
+  
+<table>
+	<?php
+	
+	  ?>
+	  <tr>
+	  <td  width="100"><?php echo "".$matricula;?></td>
+	  <td  width="100"><?php echo "".$appat;?></td>
+	  <td  width="100"><?php echo "".$apmat;?></td>
+	  <td  width="100"><?php echo "".$nombre;?></td>
+	 
+	  </tr>
+	
+
+</table>
+</body>
+</html>
